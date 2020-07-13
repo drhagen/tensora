3e382f6c995dd539d555fcc74ba47e4c8f3053cb
David Hagen <david@drhagen.com>
2020-07-13 11:47:17 -0400

    Support Python 3.8

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     cmdclass={
