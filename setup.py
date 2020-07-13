df1bdbaf3e1290e5ba95f0bcb236322848efc6c0
David Hagen <david@drhagen.com>
2020-07-13 11:57:55 -0400

    Bumpy version number to 0.0.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class TensoraBdistWheel(bdist_wheel):
 
 setup(
     name='tensora',
-    version='0.0.2',
+    version='0.0.3',
 
     description='Library for dense and sparse tensors built on the tensor algebra compiler.',
     long_description=Path('README.md').read_text(encoding='utf-8'),
