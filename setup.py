d25ff5c8135498f41d789bf4c72f6be876dbfd7d
David Hagen <david@drhagen.com>
2022-04-06 06:43:22 -0400

    Add Python 3.0 support

--- setup.py
+++ setup.py
@@ -76,6 +76,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     cmdclass={
