3251668f04bd875f09fa6009cfdcf2d69a1f711b
David Hagen <david@drhagen.com>
2023-11-25 14:01:11 -0500

    Resurrect testing on Mac OS (#18)

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+import platform
+
 from nox import Session, options, parametrize, session
 
 options.sessions = ["test", "test_numpy", "coverage", "lint"]
@@ -6,14 +8,14 @@ options.sessions = ["test", "test_numpy", "coverage", "lint"]
 @session(python=["3.10", "3.11"])
 def test(s: Session):
     s.install(".", "pytest", "pytest-cov")
-    s.env["COVERAGE_FILE"] = f".coverage.{s.python}"
+    s.env["COVERAGE_FILE"] = f".coverage.{s.python}.{platform.system()}"
     s.run("python", "-m", "pytest", "--cov", "tensora", "tests")
 
 
 @session(python=["3.10", "3.11"])
 def test_numpy(s: Session):
     s.install(".[numpy]", "pytest", "pytest-cov")
-    s.env["COVERAGE_FILE"] = f".coverage.pandas.{s.python}"
+    s.env["COVERAGE_FILE"] = f".coverage.numpy.{s.python}.{platform.system()}"
     s.run("python", "-m", "pytest", "--cov", "tensora", "tests/test_numpy.py")
 
 
