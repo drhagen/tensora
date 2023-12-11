1b1da3e79346c26651bab26e75050da3e34cc9dd
David Hagen <david@drhagen.com>
2023-12-10 20:18:55 -0500

    Assume away null bytes in CLI fuzz test

--- fuzz_tests/test_cli.py
+++ fuzz_tests/test_cli.py
@@ -1,4 +1,4 @@
-from hypothesis import given
+from hypothesis import assume, given
 from hypothesis import strategies as st
 from typer.testing import CliRunner
 
@@ -9,4 +9,7 @@ runner = CliRunner()
 
 @given(command=st.lists(st.text()))
 def test_cli_cannot_crash(command):
+    # Arguments to a CLI cannot contain null bytes.
+    assume(not any("\0" in string for string in command))
+
     _ = runner.invoke(app, command, catch_exceptions=False)
