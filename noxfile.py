b380830dc01c595dee5d63ec7a65aecc6e046bc7
David Hagen <david@drhagen.com>
2025-05-16 17:04:59 -0400

    Add support for Python 3.13

--- noxfile.py
+++ noxfile.py
@@ -4,28 +4,28 @@ from nox_poetry import Session, session
 options.sessions = ["test", "test_taco", "test_cffi", "test_numpy", "coverage", "lint"]
 
 
-@session(python=["3.10", "3.11", "3.12"])
+@session(python=["3.10", "3.11", "3.12", "3.13"])
 def test(s: Session):
     s.install(".", "pytest", "pytest-cov")
     s.env["COVERAGE_FILE"] = f".coverage.{s.python}"
     s.run("python", "-m", "pytest", "--cov", "tensora", "tests")
 
 
-@session(python=["3.10", "3.11", "3.12"])
+@session(python=["3.10", "3.11", "3.12", "3.13"])
 def test_taco(s: Session):
     s.install(".[taco]", "pytest", "pytest-cov")
     s.env["COVERAGE_FILE"] = f".coverage.taco.{s.python}"
     s.run("python", "-m", "pytest", "--cov", "tensora", "tests/taco")
 
 
-@session(python=["3.10", "3.11", "3.12"])
+@session(python=["3.10", "3.11", "3.12", "3.13"])
 def test_cffi(s: Session):
     s.install(".[cffi]", "pytest", "pytest-cov")
     s.env["COVERAGE_FILE"] = f".coverage.cffi.{s.python}"
     s.run("python", "-m", "pytest", "--cov", "tensora", "tests_cffi")
 
 
-@session(python=["3.10", "3.11", "3.12"])
+@session(python=["3.10", "3.11", "3.12", "3.13"])
 def test_numpy(s: Session):
     s.install(".[numpy]", "pytest", "pytest-cov")
     s.env["COVERAGE_FILE"] = f".coverage.numpy.{s.python}"
